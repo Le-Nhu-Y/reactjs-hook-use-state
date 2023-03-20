a0c3028060c8d12dde94d2a448eeb2445d20b4e3
Le-Nhu-Y <lenhuy20021989@gmail.com>
2023-03-20 11:03:34 +0700

    Add react-hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Counter';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +21,7 @@ function App() {
         >
           Learn React
         </a>
+        <Counter/>
       </header>
     </div>
   );
